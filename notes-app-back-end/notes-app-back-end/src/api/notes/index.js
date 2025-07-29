f8d5f8013b6898e1a9f1785604189e256fb186f8
Julius <juliusarthur2005@gmail.com>
2025-07-29 17:22:16 +0700

    initial commit

---
+++ notes-app-back-end/notes-app-back-end/src/api/notes/index.js
@@ -0,0 +1,11 @@
+const NotesHandler = require("./handle");
+const routes = require("./routes");
+
+module.exports = {
+    name: 'notes',
+    version: '1.0.0',
+    register: async (server, { service, validator }) => {
+        const notesHandler = new NotesHandler(service, validator);
+        server.route(routes(notesHandler))
+    }
+}
